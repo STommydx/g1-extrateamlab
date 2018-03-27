ba98aefbed2eb1622639ef33cbe301b6872e4661
STommydx <ttoo74@gmail.com>
2018-03-27 16:41:28 +0800

    Added timer

--- app/src/main/java/com/company/g1/g1extrateamlab/GameActivity.java
+++ app/src/main/java/com/company/g1/g1extrateamlab/GameActivity.java
@@ -1,38 +1,78 @@
 package com.company.g1.g1extrateamlab;
 
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class GameActivity extends AppCompatActivity {
 
-	GameRound mGameRound = null;
+	private GameRound mGameRound;
+
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	private SensorListener mSensorListener;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
 
+		setupSensor();
+
 		mGameRound = new GameRound(this);
 		mGameRound.start();
 	}
 
+	private void setupSensor() {
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		assert mSensorManager != null;
+		if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+			mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		}
+
+		mSensorListener = new SensorListener();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorListener.stopSensor();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorListener.startSensor();
+	}
 
 	public class GameRound {
 
 		private static final int NO_OF_LANES = 3;
+		private static final int GAME_TICKS = 600;
 
 		private ImageView player;
 		private TimerTask ticker;
 		private Timer mTimer;
 
+		private int ticks;
+
+		private Context mContext;
+
 		public GameRound(Context context) {
+			mContext = context;
+
 			player = new ImageView(context);
 			ConstraintLayout layout = findViewById(R.id.game_layout);
 
@@ -51,11 +91,14 @@ public class GameActivity extends AppCompatActivity {
 
 		public void start() {
 			mTimer = new Timer();
+			ticks = 0;
 			mTimer.scheduleAtFixedRate(ticker, 0, 50);
 		}
 
 		public void stop() {
 			mTimer.cancel();
+			startActivity(new Intent(mContext, GameoverActivity.class));
+			finish();
 		}
 
 		public void setPlayerLane(int x) {
@@ -73,7 +116,43 @@ public class GameActivity extends AppCompatActivity {
 		}
 
 		private void tick() {
+			int timeRemaining = GAME_TICKS - ticks;
+			String remString = getString(R.string.time_remain, timeRemaining / 20.0);
+			TextView mTextView = findViewById(R.id.textView);
+			mTextView.post(() -> mTextView.setText(remString));
 
+			ticks++;
+			if (ticks >= GAME_TICKS) stop();
+		}
+
+	}
+
+	private class SensorListener implements SensorEventListener {
+
+		private static final float THRESHOLD = 3.0f;
+
+		@Override
+		public void onSensorChanged(SensorEvent sensorEvent) {
+			float acc = sensorEvent.values[0];
+			if(acc < -THRESHOLD)
+				mGameRound.setPlayerLane(2);
+			else if(acc > THRESHOLD)
+				mGameRound.setPlayerLane(0);
+			else
+				mGameRound.setPlayerLane(1);
+		}
+
+		@Override
+		public void onAccuracyChanged(Sensor sensor, int i) {
+
+		}
+
+		void startSensor() {
+			mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_GAME);
+		}
+
+		void stopSensor() {
+			mSensorManager.unregisterListener(this);
 		}
 
 	}
