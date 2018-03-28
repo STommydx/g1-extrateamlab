475671311c4ae5a88296f4453f8eb134ec36425c
Bacon <pui395@yahoo.com.hk>
2018-03-29 00:11:06 +0800

    Rubbish Story Line Page Don't expect too much again
    changelog:
    added a very dynamic background

--- app/src/main/java/com/company/g1/g1extrateamlab/StoryBehind.java
+++ app/src/main/java/com/company/g1/g1extrateamlab/StoryBehind.java
@@ -1,11 +1,16 @@
 package com.company.g1.g1extrateamlab;
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StoryBehind extends AppCompatActivity {
 
@@ -17,6 +22,28 @@ public class StoryBehind extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
         getSupportActionBar().setTitle("Story Behind");
+
+        TextView storytext = (TextView) findViewById(R.id.textView11);
+        storytext.setTextColor(Color.WHITE);
+
+        final ImageView backgroundOne = (ImageView) findViewById(R.id.background_one);
+        final ImageView backgroundTwo = (ImageView) findViewById(R.id.background_two);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setDuration(10000L);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                final float progress = (float) animation.getAnimatedValue();
+                final float width = backgroundOne.getWidth();
+                final float translationX = width * progress;
+                backgroundOne.setTranslationX(translationX);
+                backgroundTwo.setTranslationX(translationX - width);
+            }
+        });
+        animator.start();
     }
 
 }
